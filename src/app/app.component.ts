6ca3cc667e4671a3c0a19568bdbce8eedb66a7dd
Lena <Lena.Koester@akquinet.de>
2018-10-01 14:35:53 +0200

    Added routing module, used routerLink in anchor element and created routed detail view. Completed tutorial step 7.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Lenas-heroes-page';
+  title = 'Lenas Tour of Heroes';
 }
